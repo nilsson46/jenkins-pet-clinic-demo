c3e56dc304978a3d8809a3de52a11e4168f6303b
nilsson46 <nilssonsimon46@gmail.com>
2023-09-13 15:31:49 +0200

    Added a test to check that the customer is created

--- src/main/java/com/example/petclinicdemo/service/CustomerService.java
+++ src/main/java/com/example/petclinicdemo/service/CustomerService.java
@@ -1,7 +1,19 @@
 package com.example.petclinicdemo.service;
 
+import com.example.petclinicdemo.model.Customer;
+import com.example.petclinicdemo.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CustomerService {
+    private CustomerRepository customerRepository;
+
+    public CustomerService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    public Customer save(Customer customer) {
+        return customerRepository.save(customer);
+    }
 }
