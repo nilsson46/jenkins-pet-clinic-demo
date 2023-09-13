6c44861375800d0682a095ba61547cfa742298e4
nilsson46 <nilssonsimon46@gmail.com>
2023-09-13 14:11:07 +0200

    created a new branch

--- src/main/java/com/example/petclinicdemo/controller/HelloController.java
+++ src/main/java/com/example/petclinicdemo/controller/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HelloController {
     @GetMapping
     public String helloWorld(){
-       return ("Hello world");
+       return ("Hello Cat");
     }
 }
---
+++ src/test/java/com/example/petclinicdemo/controller/HelloControllerTest.java
@@ -0,0 +1,12 @@
+package com.example.petclinicdemo.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloControllerTest {
+
+    @Test
+    void helloWorld() {
+    }
+}
