1328110d1e9f9299be5384e1972f04df4a09dd66
nilsson46 <nilssonsimon46@gmail.com>
2023-09-13 14:46:33 +0200

    Trying to pass test

--- src/main/java/com/example/petclinicdemo/controller/HelloController.java
+++ src/main/java/com/example/petclinicdemo/controller/HelloController.java
@@ -2,11 +2,13 @@ package com.example.petclinicdemo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @RequestMapping("hello")
 public class HelloController {
     @GetMapping
     public String helloWorld(){
-       return ("Hello Cat");
+       return "Hello Cat";
     }
 }
--- src/test/java/com/example/petclinicdemo/controller/HelloControllerTest.java
+++ src/test/java/com/example/petclinicdemo/controller/HelloControllerTest.java
@@ -1,14 +1,26 @@
 package com.example.petclinicdemo.controller;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-class HelloControllerTest {
+public class HelloControllerTest {
 
     @Test
-    void helloWorld() {
+    void helloWorld() throws Exception {
         HelloController helloController = new HelloController();
-        assertEquals(helloController, "Hello Cat");
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(helloController).build();
+
+        mockMvc.perform(get("/hello"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string(equalTo("Hello Cat")));
     }
 }
